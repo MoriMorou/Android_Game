ee97638dc6a18aa3ed64a363e5032316891ad028
MoriMorou <Mori_morou@rocketmail.com>
2018-11-26 21:31:26 +0300

    Created the new project

---
+++ StarGame/android/src/ru/mori/morou/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.mori.morou;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.mori.morou.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
